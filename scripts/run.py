511b8570b815794f62ee74452ef5045d3ca75d9f
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-06-08 09:52:18 -0700

    Fix run bug

--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,13 @@
 import os
 
-for file in os.listdir('python/'):
+filelist = []
+
+for root, dirs, files in os.walk('python/'):
+	for file in files:
+        #append the file name to the list
+		filelist.append(os.path.join(root,file))
+  
+for file in filelist:
   os.chdir('python/')
   if file.endswith('.py'):
     os.system('python '+file)
