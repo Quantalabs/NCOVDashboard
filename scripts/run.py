2b7dcbd7b9028476464a84aff6496686a17c0fef
Quantalabs <55121845+Quantalabs@users.noreply.github.com>
2021-06-08 09:57:42 -0700

    Fix run bug

--- scripts/run.py
+++ scripts/run.py
@@ -8,7 +8,8 @@ for root, dirs, files in os.walk('python/'):
 		filelist.append(os.path.join(root,file))
   
 for file in filelist:
+  newfile = file.replace('python/', '')
   os.chdir('python/')
   if file.endswith('.py'):
-    os.system('python '+file)
+    os.system('python '+newfile)
   os.chdir('..')
